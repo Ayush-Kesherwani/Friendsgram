62bd64768c88401d96e81463cd7657689e342e6e
Ayush Kesherwani <ayushkesherwani486@gmail.com>
2025-04-20 21:56:41 +0530

    modified register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem("user"));
   const [step, setStep] = useState(1);
   const [email, setEmail] = useState("");
   const [code, setCode] = useState("");
