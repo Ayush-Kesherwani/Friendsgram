2644207d9308fcd87585336305432b7f9cc9d0d0
Ayush Kesherwani <ayushkesherwani486@gmail.com>
2025-04-26 21:51:58 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://friendsgram.vercel.app',
+  credentials: true,
+}));
 app.use(express.json());
 app.use('/posts', postRoutes)
 
