f2c5c10986de8301be51006c19963b23126ecf4b
Ayush Kesherwani <ayushkesherwani486@gmail.com>
2025-05-23 19:56:28 +0530

    modified admin

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -11,7 +11,7 @@ const AdminLogin = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}api/auth/admin/adminlogin`, { email, password });
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/admin/adminlogin`, { email, password });
       localStorage.setItem('adminToken', res.data.token);
       navigate('/admindashboard');
     } catch (err) {
