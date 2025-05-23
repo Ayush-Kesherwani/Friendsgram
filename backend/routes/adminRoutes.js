5b5b2b8caa257c67815645e385cda26d30304d8d
Ayush Kesherwani <ayushkesherwani486@gmail.com>
2025-05-23 19:50:53 +0530

    modified admin

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -4,6 +4,24 @@ import User from "../models/User.js";
 
 const router = Router();
 
+const ADMIN = {
+  email: "ayush@friendsgram.com",
+  password: "admin123",
+};
+
+router.post("/adminlogin", (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === ADMIN.email && password === ADMIN.password) {
+    const token = jwt.sign({ isAdmin: true }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    return res.json({ token });
+  } else {
+    return res.status(401).json("Invalid admin credentials");
+  }
+});
+
 router.get("/admin/users", verifyAdmin, async (req, res) => {
     const users = await User.find().select("-password");
     res.json(users);
